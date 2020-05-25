316746df8019b8b7a77ec69c24de7cd6c0e7212b
chen <1259606144@qq.com>
2020-05-25 21:02:52 +0800

    添加redis

--- user-with-hystrix/src/main/java/com/chen/movie/contorller/UserController.java
+++ user-with-hystrix/src/main/java/com/chen/movie/contorller/UserController.java
@@ -1,34 +1,26 @@
 package com.chen.movie.contorller;
 
+import com.chen.movie.annotation.RateLimiter;
 import com.chen.movie.domain.Movie;
 import com.chen.movie.feign.MovieFeign;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.chen.movie.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import com.chen.movie.util.RedisUtil;
 
 @RestController
 public class UserController {
 
+
     @Autowired
     private MovieFeign movieFeign;
-    @Autowired
-    private RedisUtil redisUtil;
 
-    @GetMapping("/user/watchMovie/{id}")
+    @GetMapping("/movie/{id}")
+    @RateLimiter(key = "ratedemo:1.0.0")
     public Movie findByUser(@PathVariable Long id) {
-
-        String name = redisUtil.get("name");
-        System.err.println(name);
-
 
         return movieFeign.findMoiveById(id);
     }
-
-
-
-
 
 }
